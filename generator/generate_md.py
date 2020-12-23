59ea757784424e6a418625cf6ddb933567cdbfed
honeyfill <shniperson.sema@gmail.com>
2020-12-23 15:08:45 +0300

    GENERATOR-1021 Refactored generate_md.py and added tests_generator.py

--- generator/generate_md.py
+++ generator/generate_md.py
@@ -1,51 +1,65 @@
-class Task:
-    def __init__(self, file):
-        title = file.readline()
-        title = title[:-1]
-        self.title = "## {} \n".format(title)
-        link = file.readline()
-        self.link = "{}".format(link)
-        self.menulink = "+ [{0}](#{1})".format(title, link.split('/')[-2])
-        self.code = []
-        self.code.append('```python\n')
-        for i in file.readlines():
-            self.code.append(i)
-        self.code.append('\n')
-        self.code.append('```\n')
-
 class MdFile:
-    def __init__(self, file):
-        self.title = file.readline()
-        self.menu = []
-        file.readline()
-        cur_line = file.readline()
-        while cur_line and cur_line[0] == '+':
-            self.menu.append(cur_line)
-            cur_line = file.readline()
-        self.code = []
-        self.code.append(cur_line)
-        for i in file.readlines():
-            self.code.append(i)
+    def __init__(self, title, link, code):
+        self.title = title
+        self.link = link
+        self.code = code
 
-SourceFile = open("source_leetcode_data.txt", 'r')
-task = Task(SourceFile)
-SourceFile.close()
+        def get_title(self):
+            return "## " + self.title.slpit(". ")[1].strip("\n")
 
-FileName = input()
-fileName = FileName.lower();
-MdHolder = open(fileName + ".md", 'r')
-mdfile = MdFile(MdHolder)
-MdHolder.close()
+        def get_link(self):
+            title = self.title.slpit(". ")[1].strip("\n")
+            link_tale = self.link.split("/")[-2]
+            return f"+ [{title}](#{link_tale})"
 
-OutputFile = open("tree.md", 'w')
-OutputFile.write('# ' + FileName + '\n\n')
-for i in range(len(mdfile.menu)):
-    OutputFile.write(mdfile.menu[i])
-OutputFile.write(task.menulink + '\n')
-for i in range(len(mdfile.code)):
-    OutputFile.write(mdfile.code[i])
-OutputFile.write(task.title + '\n')
-OutputFile.write(task.link + '\n')
-for i in task.code:
-    OutputFile.write(i)
-OutputFile.close()
+        def get_code(self):
+            return f"```python\n{self.code}\n```\n"
+
+        def get_task(self):
+            return f"{self.get_title()}\n\n{self.link}\n\n{self.get_code()}\n"
+
+
+def read_source(filename):
+    data = filename.readlines()
+    res = [data[0], data[1].strip("\n"), "".join(map(lambda x: x[8::], data[6::]))]
+    return res
+
+
+def read_old(filename):
+    data = []
+    file = open(f"leetcode/{filename})")
+    data = file.read()
+    file.close()
+    return data
+
+
+def prepare_to_write(md, data):
+    links = ''
+    tasks = ''
+    if (len(data) != 0):
+        links, tasks = data.split("<!---->\n")  # i found, that it isn't shows at md files, amazing
+    links += md.get_link()
+    tasks += md.get_task()
+    tasks.strip('\n')
+    return [links, tasks]
+
+
+def write_to_file(filename, data):
+    title = f"# {filename.split('.')[0].upper()}\n\n"
+    file = open(f"leetcode/{filename}", "w")
+    if title not in data[0]:
+        file.write(title)
+    file.write(f"{data[0]}")
+    file.write("\n<!---->\n")  # i found, that it isn't shows at md files, amazing
+    file.write(data[1])
+
+
+def main(source, lol):
+    txt_file = MdFile(read_sourсe(source))
+    md_file = read_old(lol)
+    md_file = prepare_to_write(txt_file, md_file)
+    write_to_file(lol, md_file)
+
+
+source = input()
+lol = input()
